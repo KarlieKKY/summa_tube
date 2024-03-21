bb7973844a80fa8661e39c0a4216035a1c204e21
KarlieKKY <yanndeel@gmail.com>
2024-03-21 18:31:02 +0000

    styled dashboard with input form and title

--- app/(dashboard)/dashboard/page.tsx
+++ app/(dashboard)/dashboard/page.tsx
@@ -1,53 +1,70 @@
 "use client";
+import * as z from "zod";
 
-import { Card } from "@/components/ui/card";
-import { cn } from "@/lib/utils";
-import { ArrowRight, MessageSquare } from "lucide-react";
-import { useRouter } from "next/navigation";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { MessageSquare } from "lucide-react";
+import { formSchema } from "./constants";
 
-const tools = [
-  {
-    label: "Conversation",
-    icon: MessageSquare,
-    href: "/conversation",
-    color: "text-violet-500",
-    bgColor: "bg-violet-500/10",
-  },
-];
+import Heading from "@/components/heading";
+import { Form, FormControl, FormField, FormItem } from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
 
 export default function HomePage() {
-  const router = useRouter();
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      prompt: "",
+    },
+  });
+  const isLoading = form.formState.isSubmitting;
+
+  const onSubmit = async (values: z.infer<typeof formSchema>) => {
+    console.log(values);
+  };
 
   return (
     <div>
-      <div className="mb-8 space-y-4">
-        <h2 className="text-2xl md:text-4xl font-bold text-center">
-          Explore the power of SummaTube
-        </h2>
-        <p className="text-muted-foreground font-light text-sm md:text-lg text-center">
-          Chat with the smartest AI - Experience the power of SummaTube AI
-        </p>
-      </div>
-      <div className="px-4 md:px-20 lg:px-32 space-y-4">
-        {/* {tools.map((tool) => (
-          <Card
-            onClick={() => router.push(tool.href)}
-            key={tool.href}
-            className="p-4 border-black/5 flex items-center justify-between hover:shadow-md transition cursor-pointer"
-          >
-            <div className="flex items-center gap-x-4">
-              <div className={cn("p-2 w-fit rounded-md", tool.bgColor)}>
-                <tool.icon className={cn("w-8 h-8", tool.color)} />
-              </div>
-              <div className="font-semibold">{tool.label}</div>
-            </div>
-            <ArrowRight className="w-5 h-5" />
-          </Card>
-        ))} */}
-        <h3> input youtueb link</h3>
-        <h3> start generating</h3>
-        <h3>youtube summarization</h3>
-        <h3>QA section</h3>
+      <Heading
+        title="SummaTube"
+        description="Most advanced summarization model"
+        icon={MessageSquare}
+        iconColor="text-violet-500"
+        bgColor="bg-violet-500/10"
+      />
+      <div className="px-4 lg:px-8">
+        <div>
+          <Form {...form}>
+            <form
+              onSubmit={form.handleSubmit(onSubmit)}
+              className="rounded-lg border w-full p-4 px-3 md:px-6 focus-within:shadow-sm grid grid-cols-12 gap-2"
+            >
+              <FormField
+                name="prompt"
+                render={({ field }) => (
+                  <FormItem className="col-span-12 lg:col-span-10">
+                    <FormControl className="m-0 p-0">
+                      <Input
+                        className="border-0 outline-none focus-visible:ring-0 focus-visible:ring-transparent"
+                        disabled={isLoading}
+                        placeholder="Enter a YouTube link"
+                        {...field}
+                      />
+                    </FormControl>
+                  </FormItem>
+                )}
+              />
+              <Button
+                className="col-span-12 lg:col-span-2 w-full"
+                disabled={isLoading}
+              >
+                Generate
+              </Button>
+            </form>
+          </Form>
+        </div>
+        <div className="space-y-4 mt-4">Messages Content</div>
       </div>
     </div>
   );
--- app/page.tsx
+++ app/page.tsx
@@ -5,11 +5,28 @@ import Link from "next/link";
 export default function Home() {
   return (
     <main>
-      <div className="h-full relative flex flex-col items-center space-y-4 py-4">
-        <h1>Start summarizing!</h1>
-        <Link href="dashboard">
-          <Button>Click here</Button>
-        </Link>
+      <div className="h-full py-10">
+        <div className="mb-8 space-y-4">
+          <h2 className="text-2xl md:text-4xl font-bold text-center">
+            Explore the power of SummaTube
+          </h2>
+          <p className="text-muted-foreground font-light text-sm md:text-lg text-center">
+            Chat with the smartest AI - Experience the power of SummaTube AI
+          </p>
+        </div>
+        <div className="px-4 md:px-20 lg:px-32 space-y-4">
+          <div className="h-full relative flex flex-col items-center space-y-4 py-4">
+            <h1>Start summarizing!</h1>
+            <Link href="dashboard">
+              <Button>Click here</Button>
+            </Link>
+          </div>
+          <h3> input youtueb link</h3>
+
+          <h3> start generating</h3>
+          <h3>youtube summarization</h3>
+          <h3>QA section</h3>
+        </div>
       </div>
     </main>
   );
