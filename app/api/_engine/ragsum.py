a8b8b1ed1d3c9efbc928c761d898d84da3e74d08
KarlieKKY <yanndeel@gmail.com>
2024-03-25 18:01:24 +0000

    add all functions

--- app/api/_engine/ragsum.py
+++ app/api/_engine/ragsum.py
@@ -3,6 +3,14 @@ from dotenv import load_dotenv
 from pytube import YouTube
 import whisperx
 import tempfile
+from langchain_community.document_loaders import TextLoader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_community.vectorstores import DocArrayInMemorySearch
+from langchain_openai import OpenAIEmbeddings
+from langchain_core.runnables import RunnableParallel, RunnablePassthrough
+from langchain_core.output_parsers import StrOutputParser
+from langchain_openai.chat_models import ChatOpenAI
+from langchain.prompts import ChatPromptTemplate
 
 load_dotenv()
 
@@ -10,11 +18,11 @@ OPENAI_API_KEY = os.getenv('OPENAI_API_KEY')
 YOUTUBE_VIDEO = 'https://www.youtube.com/watch?v=YOyr9Bhhaq0'
 
 
-def transcribe_youtube_video(video_link, model):
+def transcribe_youtube_video(video_link, whisper_model_size):
     youtube = YouTube(video_link)
     audio = youtube.streams.filter(only_audio=True).first()
 
-    whisperx_model = whisperx.load_model(model, device='cuda', compute_type='float16')
+    whisperx_model = whisperx.load_model(whisper_model_size, device='cuda', compute_type='float16')
 
     with tempfile.TemporaryDirectory() as tmpdir:
         file = audio.download(output_path=tmpdir)
@@ -26,3 +34,38 @@ def transcribe_youtube_video(video_link, model):
             trans_file.write(transcription)
 
     return transcription
+
+
+def transcript_to_chunks(transcript, chunk_size, chunk_overlap):
+    loader = TextLoader(transcript)
+    text_documents = loader.load()
+    text_splitter = RecursiveCharacterTextSplitter(chunk_size=chunk_size, chunk_overlap=chunk_overlap)
+    document_chunks = text_splitter.split_documents(text_documents)
+
+    return document_chunks
+
+
+def store_embeddings(document_chunks):
+    embeddings = OpenAIEmbeddings(openai_api_key=OPENAI_API_KEY)
+    vectorstore = DocArrayInMemorySearch.from_documents(document_chunks, embedding=embeddings)
+    return vectorstore
+
+
+def retrive_from_embeddings(vectorstore, openai_model, user_question):
+    retriever1 = vectorstore.as_retriever()
+    setup = RunnableParallel(context=retriever1, question=RunnablePassthrough())
+    parser = StrOutputParser()
+    model = ChatOpenAI(openai_api_key=OPENAI_API_KEY, model=openai_model if openai_model else "gpt-3.5-turbo")
+    template = """
+    Answer the question based on the context below. If you can't 
+    answer the question, reply "I don't know".
+
+    Context: {context}
+
+    Question: {question}
+    """
+
+    prompt = ChatPromptTemplate.from_template(template)
+    chain = setup | prompt | model | parser
+
+    return chain.invoke(user_question)
